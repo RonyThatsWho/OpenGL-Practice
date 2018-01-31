45c0020dd471dcab1284440957652cb1fb90bffe
RonyThatsWho <13895900+RonyThatsWho@users.noreply.github.com>
2018-01-30 23:22:22 -0500

    Vertex Buffer Class Abstraction

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,27 +5,7 @@
 #include <string>
 #include <sstream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR); //GL_NO_ERROR Guarenteed to be zero, = !glGetError()
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (" << error << "): " << function <<
-			" " << file << " line: " << line << std::endl;
-		return false;
-	}
-	return true;
-}
-
+#include "Renderer.h"
 
 struct ShaderProgramSource
 {
